569072c565388f91be944b62de89885a0022901a
ReStranger <strengerplayr@gmail.com>
2024-10-02 17:38:24 +0300

    add: lsp rename

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -95,3 +95,6 @@ map("n", "<RightMouse>", function()
   local options = vim.bo.ft == "NvimTree" and "nvimtree" or "default"
   require("menu").open(options, { mouse = true })
 end, {})
+map("n", "<leader>ra", function()
+  require("ui.renamer").open "default"
+end)
