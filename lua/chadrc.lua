8464ebb4bfe6d82aa816700726ac5fedd1eb76ad
ReStranger <strengerplayr@gmail.com>
2024-10-27 00:56:57 +0300

    rem: deprecated in chadrc

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -52,7 +52,7 @@ M.ui = {
   },
   tabufline = {
     enabled = true,
-    lazyload = true,
+    lazyload = false,
     order = { "treeOffset", "buffers", "tabs", "btns" },
     modules = nil,
   },
@@ -97,12 +97,6 @@ M.nvdash = {
   },
 
   buttons = {
-    -- { "  Find File", "Spc f f", "Telescope find_files" },
-    -- { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
-    -- { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
-    -- { "  Bookmarks", "Spc m a", "Telescope marks" },
-    -- { "  Themes", "Spc t h", "Telescope themes" },
-    -- { "  Mappings", "Spc c h", "NvCheatsheet" },
     { txt = "  Find File", keys = "Spc f f", cmd = "Telescope find_files" },
     { txt = "  Recent Files", keys = "Spc f o", cmd = "Telescope oldfiles" },
     { txt = "󰈭  Find Word", keys = "Spc f w", cmd = "Telescope live_grep" },
