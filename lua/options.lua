a26f5d82add60da0eac09f03bc32f891eee848e8
ReStranger <strengerplayr@gmail.com>
2024-11-12 16:52:22 +0300

    Add Q as q :D
    Signed-off-by: ReStranger <strengerplayr@gmail.com>

--- lua/options.lua
+++ lua/options.lua
@@ -3,6 +3,7 @@ require "nvchad.options"
 local o = vim.o
 local opt = vim.opt
 local g = vim.g
+local api = vim.api
 
 o.cursorlineopt = "both"
 opt.relativenumber = true
@@ -10,6 +11,7 @@ opt.mouse = "a"
 opt.clipboard = "unnamedplus"
 g.dap_virtual_text = true
 g.codeium_enabled = true
+api.nvim_create_user_command("Q", "q", {})
 
 vim.filetype.add {
   pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
