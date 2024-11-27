4b6c74787d13611f0815c1aa69098b2120b8795e
ReStranger <strengerplayr@gmail.com>
2024-11-27 04:51:03 +0300

    Improve options
    - Add tab settings
    - Enabled incsearch
    - Set cmp window to rounded
    
    Signed-off-by: ReStranger <strengerplayr@gmail.com>

--- lua/options.lua
+++ lua/options.lua
@@ -9,10 +9,23 @@ o.cursorlineopt = "both"
 opt.relativenumber = true
 opt.mouse = "a"
 opt.clipboard = "unnamedplus"
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.incsearch = true
 g.dap_virtual_text = true
 g.codeium_enabled = true
 api.nvim_create_user_command("Q", "q", {})
 
 vim.filetype.add {
   pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
+}
+require("cmp").setup {
+  window = {
+    completion = {
+      border = "rounded",
+    },
+    documentation = {
+      border = "rounded",
+    },
+  },
 }
