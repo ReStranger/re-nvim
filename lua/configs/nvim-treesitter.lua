c189b675475f8ebdfbc2ddfb6780b6b31461e23c
ReStranger <strengerplayr@gmail.com>
2024-12-07 00:41:58 +0300

    lua/configs/nvim-treesitter: Add more file support

--- lua/configs/nvim-treesitter.lua
+++ lua/configs/nvim-treesitter.lua
@@ -1,5 +1,32 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "lua" },
+  ensure_installed = {
+    "lua",
+    "luadoc",
+    "c",
+    "cpp",
+    "rust",
+    "python",
+    "html",
+    "css",
+    "scss",
+    "javascript",
+    "typescript",
+    "tsx",
+    "nix",
+    "bash",
+    "make",
+    "yaml",
+    "toml",
+    "hyprlang",
+
+    "markdown",
+    "markdown_inline",
+
+    "git_rebase",
+    "gitattributes",
+    "gitcommit",
+    "gitignore",
+  },
 
   sync_install = true,
 
