20601e7e9132007e265d73380394ea8f5a23e2ed
ReStranger <strengerplayr@gmail.com>
2024-12-06 23:59:33 +0300

    lua: Improve autocmds

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,24 +1,27 @@
-local autocmd = vim.api.nvim_create_autocmd
+local api = vim.api
+local g = vim.g
+local schedule = vim.schedule
+local autocmd = api.nvim_create_autocmd
 
 -- user event that loads after UIEnter + only if file buf is there
 autocmd({ "UIEnter", "BufReadPost", "BufNewFile" }, {
-  group = vim.api.nvim_create_augroup("NvFilePost", { clear = true }),
+  group = api.nvim_create_augroup("NvFilePost", { clear = true }),
   callback = function(args)
-    local file = vim.api.nvim_buf_get_name(args.buf)
-    local buftype = vim.api.nvim_get_option_value("buftype", { buf = args.buf })
+    local file = api.nvim_buf_get_name(args.buf)
+    local buftype = api.nvim_get_option_value("buftype", { buf = args.buf })
 
-    if not vim.g.ui_entered and args.event == "UIEnter" then
-      vim.g.ui_entered = true
+    if not g.ui_entered and args.event == "UIEnter" then
+      g.ui_entered = true
     end
 
-    if file ~= "" and buftype ~= "nofile" and vim.g.ui_entered then
-      vim.api.nvim_exec_autocmds("User", { pattern = "FilePost", modeline = false })
-      vim.api.nvim_del_augroup_by_name "NvFilePost"
+    if file ~= "" and buftype ~= "nofile" and g.ui_entered then
+      api.nvim_exec_autocmds("User", { pattern = "FilePost", modeline = false })
+      api.nvim_del_augroup_by_name "NvFilePost"
 
-      vim.schedule(function()
-        vim.api.nvim_exec_autocmds("FileType", {})
+      schedule(function()
+        api.nvim_exec_autocmds("FileType", {})
 
-        if vim.g.editorconfig then
+        if g.editorconfig then
           require("editorconfig").config(args.buf)
         end
       end)
