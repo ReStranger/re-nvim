04b0db2cdefeadef0d3f5e7f20431bfb55a55fdd
ReStranger <strengerplayr@gmail.com>
2024-12-08 03:24:59 +0300

    lua/mappings: Fix bufferline binds

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,7 +8,6 @@ local inspect = vim.inspect
 g.mapleader = " "
 g.maplocalleader = "\\"
 
-
 map("n", "<C-h>", "<C-w>h", { desc = "switch window left" })
 map("n", "<C-l>", "<C-w>l", { desc = "switch window right" })
 map("n", "<C-j>", "<C-w>j", { desc = "switch window down" })
@@ -21,9 +20,8 @@ map("n", "<leader>b", "<cmd>vsplit<CR>", { desc = "vsplit" })
 map("n", "<leader>x", "<cmd>close<CR>", { desc = "vsplit" })
 
 -- BufferLine
-map("n", "<Tab>", ":BufferLineCycleNext<CR>")
-map("n", "<S-Tab>", ":BufferLineCyclePrev<CR>")
-
+map("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>")
+map("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>")
 
 map("n", "<leader>tt", "<cmd>TransparentToggle<CR>", { desc = "Transparent Toggle" })
 
@@ -54,7 +52,6 @@ api.nvim_create_autocmd("LspAttach", {
     map("n", "<space>sl", function()
       print(inspect(lsp.lsp.buf.list_workspace_folders()))
     end, opts)
-
 
     map("n", "<leader>lr", lsp.buf.rename, opts)
     map({ "n", "v" }, "<leader>la", lsp.buf.code_action, opts)
