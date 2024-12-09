eb6a496b69a34de07bdbe4b714188c27bc3dcc4b
ReStranger <strengerplayr@gmail.com>
2024-12-09 14:28:56 +0300

    lua/mappings: Add comment mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -19,6 +19,10 @@ map("n", "<leader>v", "<cmd>split<CR>", { desc = "split" })
 map("n", "<leader>b", "<cmd>vsplit<CR>", { desc = "vsplit" })
 map("n", "<leader>x", "<cmd>close<CR>", { desc = "vsplit" })
 
+-- Comment
+map("n", "<leader>/", "gcc", { desc = "toggle comment", remap = true })
+map("v", "<leader>/", "gc", { desc = "toggle comment", remap = true })
+
 -- BufferLine
 map("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>")
 map("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>")
@@ -36,28 +40,28 @@ map("n", "]d", diagnostic.goto_next)
 map("n", "<leader>ld", diagnostic.setloclist)
 
 api.nvim_create_autocmd("LspAttach", {
-  group = api.nvim_create_augroup("UserLspConfig", {}),
-  callback = function(ev)
-    bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+    group = api.nvim_create_augroup("UserLspConfig", {}),
+    callback = function(ev)
+        bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-    local opts = { buffer = ev.buf }
-    map("n", "gD", lsp.buf.declaration, opts)
-    map("n", "gd", lsp.buf.definition, opts)
-    map("n", "K", lsp.buf.hover, opts)
-    map("n", "gi", lsp.buf.implementation, opts)
-    map("n", "<C-k>", lsp.buf.signature_help, opts)
+        local opts = { buffer = ev.buf }
+        map("n", "gD", lsp.buf.declaration, opts)
+        map("n", "gd", lsp.buf.definition, opts)
+        map("n", "K", lsp.buf.hover, opts)
+        map("n", "gi", lsp.buf.implementation, opts)
+        map("n", "<C-k>", lsp.buf.signature_help, opts)
 
-    map("n", "<space>sa", lsp.buf.add_workspace_folder, opts)
-    map("n", "<space>sr", lsp.buf.remove_workspace_folder, opts)
-    map("n", "<space>sl", function()
-      print(inspect(lsp.lsp.buf.list_workspace_folders()))
-    end, opts)
+        map("n", "<space>sa", lsp.buf.add_workspace_folder, opts)
+        map("n", "<space>sr", lsp.buf.remove_workspace_folder, opts)
+        map("n", "<space>sl", function()
+            print(inspect(lsp.lsp.buf.list_workspace_folders()))
+        end, opts)
 
-    map("n", "<leader>lr", lsp.buf.rename, opts)
-    map({ "n", "v" }, "<leader>la", lsp.buf.code_action, opts)
-    map("n", "gr", lsp.buf.references, opts)
-    map("n", "<leafer>lf", function()
-      lsp.buf.format { async = true }
-    end, opts)
-  end,
+        map("n", "<leader>lr", lsp.buf.rename, opts)
+        map({ "n", "v" }, "<leader>la", lsp.buf.code_action, opts)
+        map("n", "gr", lsp.buf.references, opts)
+        map("n", "<leafer>lf", function()
+            lsp.buf.format { async = true }
+        end, opts)
+    end,
 })
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,187 +1,187 @@
 require("lazy").setup {
-  -- UI/UX
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    cmp = { "Neotree float toggle", "Neotree left toggle" },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-      "s1n7ax/nvim-window-picker",
-    },
-    config = function()
-      require "configs.neo-tree"
-    end,
-  },
-  {
-    "akinsho/bufferline.nvim",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    config = function()
-      require "configs.bufferline"
-    end,
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-    event = { "BufReadPost", "BufNewFile" },
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require "configs.lualine"
-    end,
-  },
-  {
-    "luukvbaal/statuscol.nvim",
-    event = { "BufReadPost", "BufNewFile" },
-    config = function()
-      require "configs.statuscol"
-    end,
-  },
-  {
-    "kevinhwang91/nvim-ufo",
-    event = { "BufReadPost", "BufNewFile" },
-    dependencies = { "kevinhwang91/promise-async" },
-    config = function()
-      require "configs.ufo"
-    end,
-  },
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {},
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-    },
-  },
-  {
-    "nvimdev/dashboard-nvim",
-    event = "VimEnter",
-    config = function()
-      require "configs.dashboard"
-    end,
-    dependencies = { { "nvim-tree/nvim-web-devicons" } },
-  },
-  {
-    "xiyaowong/transparent.nvim",
-    cmd = "TransparentToggle",
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope-ui-select.nvim",
-    },
-    config = function()
-      require "configs.telescope"
-    end,
-  },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require "configs.catppuccin"
-    end,
-  },
-  -- Inline
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPost", "BufNewFile" },
-    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
-    build = ":TSUpdate",
-    config = function()
-      require "configs.nvim-treesitter"
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    event = "User FilePost",
-    config = function()
-      require "configs.nvim-lspconfig"
-    end,
-  },
-  {
-    "stevearc/conform.nvim",
-    event = "BufWritePre",
-    config = function()
-      require "configs.conform"
-    end,
-  },
-  {
-    "mfussenegger/nvim-lint",
-    event = "VeryLazy",
-    config = function()
-      require "configs.lint"
-    end,
-  },
-  -- Base function
-  {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-      {
-        "L3MON4D3/LuaSnip",
-        dependencies = "rafamadriz/friendly-snippets",
-        opts = { history = true, updateevents = "TextChanged,TextChangedI" },
-        config = function(_, opts)
-          require("luasnip").config.set_config(opts)
-          require "configs.luasnip"
-        end,
-      },
-      {
-        "windwp/nvim-autopairs",
-        opts = {
-          fast_wrap = {},
-          disable_filetype = { "TelescopePrompt", "vim" },
+    -- UI/UX
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        cmp = { "Neotree float toggle", "Neotree left toggle" },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim",
+            "s1n7ax/nvim-window-picker",
         },
-        config = function(_, opts)
-          require("nvim-autopairs").setup(opts)
-
-          -- setup cmp for autopairs
-          local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-          require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+        config = function()
+            require "configs.neo-tree"
         end,
-      },
-      {
-        "saadparwaiz1/cmp_luasnip",
-        "hrsh7th/cmp-nvim-lua",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "petertriho/cmp-git",
-      },
     },
-    config = function()
-      require "configs.nvim-cmp"
-    end,
-  },
-  {
-    "max397574/better-escape.nvim",
-    config = function()
-      require "configs.better-escape"
-    end,
-  },
-  -- Rust
-  {
-    "mrcjkb/rustaceanvim",
-    ft = { "rust" },
-    config = function()
-      require "configs.rustaceanvim"
-    end,
-  },
-  {
-    "saecki/crates.nvim",
-    ft = { "toml" },
-    config = function(_, opts)
-      local crates = require "crates"
-      crates.setup(opts)
-      require("cmp").setup.buffer {
-        sources = { { name = "crates" } },
-      }
-      crates.show()
-    end,
-  },
-  -- Apps
-  "eandrju/cellular-automaton.nvim",
-  cmd = "CellularAutomaton",
+    {
+        "akinsho/bufferline.nvim",
+        dependencies = "nvim-tree/nvim-web-devicons",
+        config = function()
+            require "configs.bufferline"
+        end,
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        event = { "BufReadPost", "BufNewFile" },
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require "configs.lualine"
+        end,
+    },
+    {
+        "luukvbaal/statuscol.nvim",
+        event = { "BufReadPost", "BufNewFile" },
+        config = function()
+            require "configs.statuscol"
+        end,
+    },
+    {
+        "kevinhwang91/nvim-ufo",
+        event = { "BufReadPost", "BufNewFile" },
+        dependencies = { "kevinhwang91/promise-async" },
+        config = function()
+            require "configs.ufo"
+        end,
+    },
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {},
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+        },
+    },
+    {
+        "nvimdev/dashboard-nvim",
+        event = "VimEnter",
+        config = function()
+            require "configs.dashboard"
+        end,
+        dependencies = { { "nvim-tree/nvim-web-devicons" } },
+    },
+    {
+        "xiyaowong/transparent.nvim",
+        cmd = "TransparentToggle",
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope-ui-select.nvim",
+        },
+        config = function()
+            require "configs.telescope"
+        end,
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            require "configs.catppuccin"
+        end,
+    },
+    -- Inline
+    {
+        "nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPost", "BufNewFile" },
+        cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+        build = ":TSUpdate",
+        config = function()
+            require "configs.nvim-treesitter"
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        event = "User FilePost",
+        config = function()
+            require "configs.nvim-lspconfig"
+        end,
+    },
+    {
+        "stevearc/conform.nvim",
+        event = "BufWritePre",
+        config = function()
+            require "configs.conform"
+        end,
+    },
+    {
+        "mfussenegger/nvim-lint",
+        event = "VeryLazy",
+        config = function()
+            require "configs.lint"
+        end,
+    },
+    -- Base function
+    {
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
+        dependencies = {
+            {
+                "L3MON4D3/LuaSnip",
+                dependencies = "rafamadriz/friendly-snippets",
+                opts = { history = true, updateevents = "TextChanged,TextChangedI" },
+                config = function(_, opts)
+                    require("luasnip").config.set_config(opts)
+                    require "configs.luasnip"
+                end,
+            },
+            {
+                "windwp/nvim-autopairs",
+                opts = {
+                    fast_wrap = {},
+                    disable_filetype = { "TelescopePrompt", "vim" },
+                },
+                config = function(_, opts)
+                    require("nvim-autopairs").setup(opts)
+
+                    -- setup cmp for autopairs
+                    local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+                    require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+                end,
+            },
+            {
+                "saadparwaiz1/cmp_luasnip",
+                "hrsh7th/cmp-nvim-lua",
+                "hrsh7th/cmp-nvim-lsp",
+                "hrsh7th/cmp-buffer",
+                "hrsh7th/cmp-path",
+                "hrsh7th/cmp-cmdline",
+                "petertriho/cmp-git",
+            },
+        },
+        config = function()
+            require "configs.nvim-cmp"
+        end,
+    },
+    {
+        "max397574/better-escape.nvim",
+        config = function()
+            require "configs.better-escape"
+        end,
+    },
+    -- Rust
+    {
+        "mrcjkb/rustaceanvim",
+        ft = { "rust" },
+        config = function()
+            require "configs.rustaceanvim"
+        end,
+    },
+    {
+        "saecki/crates.nvim",
+        ft = { "toml" },
+        config = function(_, opts)
+            local crates = require "crates"
+            crates.setup(opts)
+            require("cmp").setup.buffer {
+                sources = { { name = "crates" } },
+            }
+            crates.show()
+        end,
+    },
+    -- Apps
+    "eandrju/cellular-automaton.nvim",
+    cmd = "CellularAutomaton",
 }
