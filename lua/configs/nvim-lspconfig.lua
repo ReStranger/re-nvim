7b65749ff8f1216929012687ce785614b41472bb
ReStranger <strengerplayr@gmail.com>
2024-12-11 17:21:19 +0300

    lua/configs/nvim-lspconfig: Add rounded border to hover window

--- lua/configs/nvim-lspconfig.lua
+++ lua/configs/nvim-lspconfig.lua
@@ -1,5 +1,33 @@
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+local border = {
+    { "╭", "FloatBorder" },
+
+    { "─", "FloatBorder" },
+    { "╮", "FloatBorder" },
+    { "│", "FloatBorder" },
+    { "╯", "FloatBorder" },
+    { "─", "FloatBorder" },
+
+    { "╰", "FloatBorder" },
+
+    { "│", "FloatBorder" },
+}
+
+-- LSP settings (for overriding per client)
+local handlers = {
+    ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
+    ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
+}
+
+-- To instead override globally
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+    opts = opts or {}
+    opts.border = opts.border or border
+    return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
+
 local lspconfig = require "lspconfig"
 local api = vim.api
 local buf = vim.buf
@@ -20,6 +48,7 @@ local servers = {
 -- lsps with default config
 for _, a in ipairs(servers) do
     lspconfig[a].setup {
+        handlers = handlers,
         capabilities = capabilities,
     }
 end
