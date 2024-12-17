942cb73dc5a0421d3cc673c231da1b3253865fc4
ReStranger <strengerplayr@gmail.com>
2024-12-17 17:49:17 +0300

    lua/configs/dap: Add initial support for js/ts debuger
    Signed-off-by: ReStranger <strengerplayr@gmail.com>

--- lua/configs/dap.lua
+++ lua/configs/dap.lua
@@ -18,3 +18,24 @@ dap.adapters.lldb = {
     command = "lldb",
     name = "lldb",
 }
+dap.adapters["pwa-node"] = {
+    type = "server",
+    host = "127.0.0.1",
+    port = 8123,
+    executable = {
+        command = "js-debug-adapter",
+    },
+}
+
+for _, language in ipairs { "typescript", "javascript" } do
+    dap.configurations[language] = {
+        {
+            type = "pwa-node",
+            request = "launch",
+            name = "Launch file",
+            program = "${file}",
+            cwd = "${workspaceFolder}",
+            runtimeExecutable = "node",
+        },
+    }
+end
