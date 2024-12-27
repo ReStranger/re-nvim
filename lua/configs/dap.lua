0968bceeaf107cdac24b6e95c4c7c4940b5ca73e
ReStranger <strengerplayr@gmail.com>
2024-12-27 15:49:04 +0300

    lua/configs: Improve c/c++/rust debug tool
    Signed-off-by: ReStranger <strengerplayr@gmail.com>

--- lua/configs/dap.lua
+++ lua/configs/dap.lua
@@ -1,41 +1,20 @@
 local dap = require "dap"
+dap.adapters.codelldb = {
+    type = "executable",
+    command = "codelldb",
+}
+
 dap.configurations.cpp = {
     {
-        name = "Launch",
-        type = "lldb",
+        name = "Launch file",
+        type = "codelldb",
         request = "launch",
         program = function()
             return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
         end,
         cwd = "${workspaceFolder}",
         stopOnEntry = false,
-        args = {},
-        runInTerminal = true,
     },
 }
-dap.adapters.lldb = {
-    type = "executable",
-    command = "lldb",
-    name = "lldb",
-}
-dap.adapters["pwa-node"] = {
-    type = "server",
-    host = "127.0.0.1",
-    port = 8123,
-    executable = {
-        command = "js-debug-adapter",
-    },
-}
-
-for _, language in ipairs { "typescript", "javascript" } do
-    dap.configurations[language] = {
-        {
-            type = "pwa-node",
-            request = "launch",
-            name = "Launch file",
-            program = "${file}",
-            cwd = "${workspaceFolder}",
-            runtimeExecutable = "node",
-        },
-    }
-end
+dap.configurations.c = dap.configurations.cpp
+dap.configurations.rust = dap.configurations.cpp
