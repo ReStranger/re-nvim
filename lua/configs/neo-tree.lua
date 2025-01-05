912189f68f60e1bfa58f91c9cc7a0b92db070bee
ReStranger <strengerplayr@gmail.com>
2025-01-05 19:52:36 +0300

    lua/configs: Improve neo-tree setup
    Signed-off-by: ReStranger <strengerplayr@gmail.com>

--- lua/configs/neo-tree.lua
+++ lua/configs/neo-tree.lua
@@ -1,6 +1,190 @@
-local sing_define = vim.fn.sign_define
+local fn = vim.fn
 
-sing_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-sing_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-sing_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-sing_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+
+require("neo-tree").setup {
+    close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
+    popup_border_style = "rounded",
+    enable_git_status = true,
+    enable_diagnostics = true,
+    open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
+    sort_case_insensitive = false, -- used when sorting files and directories in the tree
+    sort_function = nil, -- use a custom function for sorting files and directories in the tree
+    -- sort_function = function (a,b)
+    --       if a.type == b.type then
+    --           return a.path > b.path
+    --       else
+    --           return a.type > b.type
+    --       end
+    --   end , -- this sorts files and directories descendantly
+    default_component_configs = {
+        container = {
+            enable_character_fade = true,
+        },
+        indent = {
+            indent_size = 2,
+            padding = 1, -- extra padding on left hand side
+            -- indent guides
+            with_markers = true,
+            indent_marker = "│",
+            last_indent_marker = "└",
+            highlight = "NeoTreeIndentMarker",
+            -- expander config, needed for nesting files
+            with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
+            expander_collapsed = "",
+            expander_expanded = "",
+            expander_highlight = "NeoTreeExpander",
+        },
+        icon = {
+            folder_closed = "",
+            folder_open = "",
+            folder_empty = "󰜌",
+            provider = function(icon, node, state) -- default icon provider utilizes nvim-web-devicons if available
+                if node.type == "file" or node.type == "terminal" then
+                    local success, web_devicons = pcall(require, "nvim-web-devicons")
+                    local name = node.type == "terminal" and "terminal" or node.name
+                    if success then
+                        local devicon, hl = web_devicons.get_icon(name)
+                        icon.text = devicon or icon.text
+                        icon.highlight = hl or icon.highlight
+                    end
+                end
+            end,
+            -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
+            -- then these will never be used.
+            default = "*",
+            highlight = "NeoTreeFileIcon",
+        },
+        modified = {
+            symbol = "[+]",
+            highlight = "NeoTreeModified",
+        },
+        name = {
+            trailing_slash = false,
+            use_git_status_colors = true,
+            highlight = "NeoTreeFileName",
+        },
+        git_status = {
+            symbols = {
+                -- Change type
+                added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+                modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
+                deleted = "✖", -- this can only be used in the git_status source
+                renamed = "󰁕", -- this can only be used in the git_status source
+                -- Status type
+                untracked = "",
+                ignored = "",
+                unstaged = "󰄱",
+                staged = "",
+                conflict = "",
+            },
+        },
+        -- If you don't want to use these columns, you can set `enabled = false` for each of them individually
+        file_size = {
+            enabled = true,
+            width = 12, -- width of the column
+            required_width = 64, -- min width of window required to show this column
+        },
+        type = {
+            enabled = true,
+            width = 10, -- width of the column
+            required_width = 122, -- min width of window required to show this column
+        },
+        last_modified = {
+            enabled = true,
+            width = 20, -- width of the column
+            required_width = 88, -- min width of window required to show this column
+        },
+        created = {
+            enabled = true,
+            width = 20, -- width of the column
+            required_width = 110, -- min width of window required to show this column
+        },
+        symlink_target = {
+            enabled = false,
+        },
+    },
+    window = {
+        position = "left",
+        width = 35,
+        mapping_options = {
+            noremap = true,
+            nowait = true,
+        },
+    },
+    nesting_rules = {},
+    filesystem = {
+        filtered_items = {
+            visible = false, -- when true, they will just be displayed differently than normal items
+            hide_dotfiles = false,
+            hide_gitignored = false,
+            hide_by_name = {
+                --"node_modules"
+            },
+            hide_by_pattern = { -- uses glob style patterns
+                --"*.meta",
+                --"*/src/*/tsconfig.json",
+            },
+            always_show = { -- remains visible even if other settings would normally hide it
+                --".gitignored",
+            },
+            always_show_by_pattern = { -- uses glob style patterns
+                --".env*",
+            },
+            never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+                --".DS_Store",
+                --"thumbs.db"
+            },
+            never_show_by_pattern = { -- uses glob style patterns
+                --".null-ls_*",
+            },
+        },
+        follow_current_file = {
+            enabled = false, -- This will find and focus the file in the active buffer every time
+            --               -- the current file is changed while the tree is open.
+            leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+        },
+        group_empty_dirs = false, -- when true, empty folders will be grouped together
+        hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
+        -- in whatever position is specified in window.position
+        -- "open_current",  -- netrw disabled, opening a directory opens within the
+        -- window like netrw would, regardless of window.position
+        -- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
+        use_libuv_file_watcher = false, -- This will use the OS level file watchers to detect changes
+        -- instead of relying on nvim autocmd events.
+        window = {
+            mappings = {
+                ["<bs>"] = "navigate_up",
+                ["."] = "set_root",
+                ["H"] = "toggle_hidden",
+                ["/"] = "fuzzy_finder",
+                ["D"] = "fuzzy_finder_directory",
+                ["#"] = "fuzzy_sorter", -- fuzzy sorting using the fzy algorithm
+                -- ["D"] = "fuzzy_sorter_directory",
+                ["f"] = "filter_on_submit",
+                ["<c-x>"] = "clear_filter",
+                ["[g"] = "prev_git_modified",
+                ["]g"] = "next_git_modified",
+                ["o"] = { "show_help", nowait = false, config = { title = "Order by", prefix_key = "o" } },
+                ["oc"] = { "order_by_created", nowait = false },
+                ["od"] = { "order_by_diagnostics", nowait = false },
+                ["og"] = { "order_by_git_status", nowait = false },
+                ["om"] = { "order_by_modified", nowait = false },
+                ["on"] = { "order_by_name", nowait = false },
+                ["os"] = { "order_by_size", nowait = false },
+                ["ot"] = { "order_by_type", nowait = false },
+                -- ['<key>'] = function(state) ... end,
+            },
+            fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
+                ["<down>"] = "move_cursor_down",
+                ["<C-n>"] = "move_cursor_down",
+                ["<up>"] = "move_cursor_up",
+                ["<C-p>"] = "move_cursor_up",
+                -- ['<key>'] = function(state, scroll_padding) ... end,
+            },
+        },
+    },
+}
