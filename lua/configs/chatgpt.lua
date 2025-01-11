4950a5ee971d52ee1957c78419182aaf0bc3b543
ReStranger <strengerplayr@gmail.com>
2025-01-11 19:11:23 +0300

    lua/configs: Fix chatgpt plugin for o1-preview
    Signed-off-by: ReStranger <strengerplayr@gmail.com>

--- lua/configs/chatgpt.lua
+++ lua/configs/chatgpt.lua
@@ -1,23 +1,12 @@
 require("chatgpt").setup {
     -- this config assumes you have OPENAI_API_KEY environment variable set
     openai_params = {
-        -- NOTE: model can be a function returning the model name
-        -- this is useful if you want to change the model on the fly
-        -- using commands
-        -- Example:
-        -- model = function()
-        --     if some_condition() then
-        --         return "gpt-4-1106-preview"
-        --     else
-        --         return "gpt-3.5-turbo"
-        --     end
-        -- end,
-        model = "01-preview",
+        model = "o1-preview",
         frequency_penalty = 0,
         presence_penalty = 0,
         max_tokens = 4095,
-        temperature = 0.2,
-        top_p = 0.1,
+        temperature = 1,
+        top_p = 1,
         n = 1,
     },
 }
