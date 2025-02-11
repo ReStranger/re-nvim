ae29f42cf8b6c715454edb3e8102c672a0bafa66
ReStranger <restranger@disroot.org>
2025-02-12 00:10:23 +0300

    fix(better-escape): Visual mode bind
    Signed-off-by: ReStranger <restranger@disroot.org>

--- lua/configs/better-escape.lua
+++ lua/configs/better-escape.lua
@@ -2,10 +2,10 @@ require("better_escape").setup {
     timeout = 200,
     default_mappings = false,
     mappings = {
-        i = { j = { j = "<Esc>" }, ["о"] = { ["о"] = "<esc>" } },
+        i = { j = { j = "<Esc>" }, ["о"] = { ["о"] = "<Esc>" } },
         c = { j = { j = "<Esc>" } },
         t = { j = { j = "<C-\\><C-n>" } },
-        v = { j = { ["<leader>"] = "<Esc>" } },
-        s = {},
+        v = { ["<C-j>"] = { ["<C-j>"] = "<Esc>" } },
+        s = { j = { j = "<Esc>" } },
     },
 }
