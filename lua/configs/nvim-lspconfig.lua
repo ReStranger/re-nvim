86918f23ce7995efeb006a8b5c0ba3cfd58a42a7
ReStranger <restranger@disroot.org>
2025-02-21 00:57:06 +0300

    feat(lspconfig): Enable clang-tidy for clangd
    Signed-off-by: ReStranger <restranger@disroot.org>

--- lua/configs/nvim-lspconfig.lua
+++ lua/configs/nvim-lspconfig.lua
@@ -73,7 +73,14 @@ lspconfig.pyright.setup {
     },
 }
 
-lspconfig.clangd.setup { cmd = { "clangd", "--compile-commands-dir=." } }
+lspconfig.clangd.setup {
+    cmd = {
+        "clangd",
+        "--clang-tidy",
+        "--clang-tidy-checks='google-,modernize-,performance-*'",
+        "--compile-commands-dir=.",
+    },
+}
 
 local function organize_imports()
     local params = {
