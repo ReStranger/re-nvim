8a8db24c651ac67976d17bf2bd4839ecafbf7114
ReStranger <restranger@disroot.org>
2025-10-01 16:48:45 +0300

    chore(codecompanion): Switch to `adapters.http`
    Signed-off-by: ReStranger <restranger@disroot.org>

--- lua/configs/codecompanion.lua
+++ lua/configs/codecompanion.lua
@@ -24,20 +24,22 @@ require("codecompanion").setup {
         },
     },
     adapters = {
-        weegam_openai = function()
-            return require("codecompanion.adapters").extend("openai_compatible", {
-                env = {
-                    url = "https://oai.weegam.com/proxy/openai", -- optional: default value is ollama url http://127.0.0.1:11434
-                    api_key = "OPENAI_API_KEY", -- optional: if your endpoint is authenticated
-                    chat_url = "/v1/chat/completions", -- optional: default value, override if different
-                    models_endpoint = "/v1/models", -- optional: attaches to the end of the URL to form the endpoint to retrieve models
-                },
-                schema = {
-                    model = {
-                        default = "gpt-4.1", -- define llm model to be used
+        http = {
+            weegam_openai = function()
+                return require("codecompanion.adapters").extend("openai_compatible", {
+                    env = {
+                        url = "https://oai.weegam.com/proxy/openai", -- optional: default value is ollama url http://127.0.0.1:11434
+                        api_key = "OPENAI_API_KEY", -- optional: if your endpoint is authenticated
+                        chat_url = "/v1/chat/completions", -- optional: default value, override if different
+                        models_endpoint = "/v1/models", -- optional: attaches to the end of the URL to form the endpoint to retrieve models
                     },
-                },
-            })
-        end,
+                    schema = {
+                        model = {
+                            default = "gpt-4.1", -- define llm model to be used
+                        },
+                    },
+                })
+            end,
+        },
     },
 }
