4a03c16723ae2357b7efb5c1ee54a99c53e957a1
ReStranger <restranger@disroot.org>
2026-02-08 14:29:00 +0300

    chore(telescope): Update keybinds
    Signed-off-by: ReStranger <restranger@disroot.org>

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -12,10 +12,8 @@ require("telescope").setup {
     map("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" }),
 
     -- LSP
-    map("n", "<leader>fr", builtin.lsp_references, { desc = "Telescope find files" }),
-    map("n", "<leader>fg", builtin.lsp_incoming_calls, { desc = "Telescope live grep" }),
-    map("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" }),
-    map("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" }),
+    map("n", "<leader>fr", builtin.lsp_references, { desc = "Telescope LSP references" }),
+    map("n", "<leader>fg", builtin.lsp_incoming_calls, { desc = "Telescope LSP incoming calls" }),
 
     map(
         "n",
@@ -26,6 +24,16 @@ require("telescope").setup {
     map("n", "<leader>cm", builtin.git_commits, { desc = "Telescope git commits" }),
     map("n", "<leader>gt", builtin.git_status, { desc = "Telescope git status" }),
 
+    defaults = {
+        mappings = {
+            n = {
+                ["q"] = require("telescope.actions").close,
+            },
+            i = {
+                ["<C-h>"] = "which_key",
+            },
+        },
+    },
     extensions = {
         ["ui-select"] = {
             require("telescope.themes").get_dropdown {},
