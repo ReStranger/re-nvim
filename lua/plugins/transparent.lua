75ea7b85a0072bfe1fb01942040148ab5adf314a
ReStranger <restranger@disroot.org>
2026-09-04 00:33:45 +0300

    feat(plugins): add transparent plugin setup
    Signed-off-by: ReStranger <restranger@disroot.org>

---
+++ lua/plugins/transparent.lua
@@ -0,0 +1,33 @@
+return {
+    "xiyaowong/transparent.nvim",
+    lazy = false,
+    dependencies = { "akinsho/bufferline.nvim" },
+    opts = {
+        extra_groups = {
+            "NormalFloat",
+            "FloatBorder",
+            "GitSignsAdd",
+            "GitSignsChange",
+            "GitSignsDelete",
+            "GitSignsAddNr",
+            "GitSignsChangeNr",
+            "GitSignsDeleteNr",
+            "GitSignsAddLn",
+            "GitSignsChangeLn",
+            "GitSignsDeleteLn",
+            "GitSignsCurrentLineBlame",
+        },
+        exclude_groups = {},
+        on_clear = function() end,
+    },
+    config = function(_, opts)
+        require("transparent").setup(opts)
+    end,
+    keys = {
+        {
+            "<leader>tt",
+            "<cmd>TransparentToggle<cr>",
+            desc = "Transparent Toggle",
+        },
+    },
+}
