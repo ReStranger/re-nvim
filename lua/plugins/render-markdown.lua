c13edf2bd5f67bee494de802d67365844f23493b
ReStranger <restranger@disroot.org>
2026-09-04 00:27:20 +0300

    feat(render-markdown): load plugin only in md files
    Signed-off-by: ReStranger <restranger@disroot.org>

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,5 +1,6 @@
 return {
     "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown", "markdown_inline" },
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
     opts = {
         completions = { lsp = { enabled = true } },
